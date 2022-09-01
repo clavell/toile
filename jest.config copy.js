b3e3eada9c2d81d5eca0a880e87cb5e51e4a68bc
clavell <christopher.lavell@yahoo.ca>
2022-08-31 21:38:03 -0700

    file cleanup

--- jest.config copy.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: '@vue/cli-plugin-unit-jest',
-  transform: {
-    '^.+\\.vue$': 'vue-jest',
-    "\\.(gql|graphql)$": "jest-transform-graphql",
-      ".*": "babel-jest"
-  },
-}
